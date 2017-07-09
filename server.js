54e8bff8d63aff037380350ad9ad4240513ab057
L.Carquet <laetitiacarquet@gmail.com>
2017-07-09 11:58:13 +0200

    set routes for demo data through public file

--- server.js
+++ server.js
@@ -4,8 +4,11 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
+const CollaboratorsDemoData = require('./public/collaborators.json')
+const Collaborator_idDemoData = require('./public/collaborator_id.json')
+const ProjectsDemoData = require('./public/projects.json')
+const Project_idDemoData = require('./public/project_id.json')
 // const api = require('./routes/api')
 
 //launch server
@@ -31,6 +34,22 @@ app.use(express.static('public'));
 //render the html index file from public 
 app.get('/', (req, res) =>{
 	res.sendFile('./public/index.html')
+})
+
+app.get('/api/v1/collaborators', (req, res) =>{
+	res.send(JSON.stringify(CollaboratorsDemoData))
+})
+
+app.get('/api/v1/collaborator/:id', (req, res) =>{
+	res.send(JSON.stringify(Collaborator_idDemoData))
+})
+
+app.get('/api/v1/projects', (req, res) =>{
+	res.send(JSON.stringify(ProjectsDemoData))
+})
+
+app.get('/api/v1/project/:id', (req, res) =>{
+	res.send(JSON.stringify(Project_idDemoData))
 })
 
 
