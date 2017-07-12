146d25085e6210fc7b9c0552cbe39a78d9a29356
L.Carquet <laetitiacarquet@gmail.com>
2017-07-12 18:08:18 +0200

    all endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,15 @@
 'use strict'
 
 const express = require('express')
+// const collaboratorsCTRL = require('../controllers/collaborators')
 // const CollaboratorCtrl = require('../controllers/collaborators')
 // const ProjectCtrl = require('../controllers/projects')
 
-const api = express.Router()
+// const api = express.Router()
 
 
 // set home page
-api.get('/', (req, res) =>{
-	res.sendFile('./public/index.html')
-})
+
 
 // routes for collaborators data
 
--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 'use strict'
 
 //here you can find anything related to configuration
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-
-
 // const api = require('./routes/api')
 const collaboratorsCTRL = require('./controllers/collaborators')
 const projectsCTRL = require('./controllers/projects')
 
-//middleware
+//MIDDLEWARE
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
@@ -22,9 +19,13 @@ app.use( (req, res, next) => {
   next();
 });
 
+//Access to public file
 app.use(express.static('public'));
 
 // ENDPOINTS
+app.get('/', (req, res) =>{
+	res.sendFile('./public/index.html')
+})
 app.get('/api/v1/collaborators', collaboratorsCTRL.getCollaborators)
 app.get('/api/v1/collaborator/:id', collaboratorsCTRL.getCollaborator)
 app.get('/api/v1/projects', projectsCTRL.getProjects)
